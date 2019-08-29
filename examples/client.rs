e09f28c90fc5d3c3f7a528b01245b2b036476b1c
Remi Bernotavicius <remi@kunugiken.com>
2019-08-28 20:35:00 -0700

    Flesh out the client example some

--- examples/client.rs
+++ examples/client.rs
@@ -1,15 +1,24 @@
-use std::io;
-
 use http_io::error::Result;
+use http_io::protocol::HttpMethod;
 use http_io::url::Url;
+use std::io;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(long = "method", default_value = "GET")]
+    method: HttpMethod,
+    #[structopt(long = "data", default_value = "")]
+    data: String,
+    url: Url,
+}
 
 fn main() -> Result<()> {
-    let args = std::env::args();
-    let url: Url = args
-        .skip(1)
-        .next()
-        .unwrap_or("http://www.google.com".into())
-        .parse()?;
-    io::copy(&mut http_io::client::get(url)?, &mut io::stdout())?;
+    let opts = Options::from_args();
+    let mut body = match opts.method {
+        HttpMethod::Get => http_io::client::get(opts.url)?,
+        HttpMethod::Put => http_io::client::put(opts.url, opts.data.as_bytes())?,
+    };
+    io::copy(&mut body, &mut io::stdout())?;
     Ok(())
 }
